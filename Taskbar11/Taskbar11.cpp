1deded8aa4f8d8cf775aefd5128543ebc9776e17
Chris Andriessen <50437199+ChrisAnd1998@users.noreply.github.com>
2022-01-02 08:31:31 +0100

    Add files via upload

--- Taskbar11/Taskbar11.cpp
+++ Taskbar11/Taskbar11.cpp
@@ -36,15 +36,20 @@ int main() //WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR 
                 if (wcscmp(title, L"Shell_TrayWnd") == 0) {
 
                     HWND Shell_TrayWnd = tb;
-                    //HWND Start = FindWindowEx(Shell_TrayWnd, 0, L"Start", NULL);
+                    HWND Start = FindWindowEx(Shell_TrayWnd, 0, L"Start", NULL);
                     HWND RebarWindow32 = FindWindowEx(Shell_TrayWnd, 0, L"RebarWindow32", NULL);
                     HWND MSTaskSwWClass = FindWindowEx(RebarWindow32, 0, L"MSTaskSwWClass", NULL);
+                    HWND TrayNotifyWnd = FindWindowEx(Shell_TrayWnd, 0, L"TrayNotifyWnd", NULL);
+
 
                     RECT rect_Shell_TrayWnd;
                     GetWindowRect(Shell_TrayWnd, &rect_Shell_TrayWnd);
 
-                    //RECT rect_Start;
-                    //GetWindowRect(Start, &rect_Start);
+                    RECT rect_Start;
+                    GetWindowRect(Start, &rect_Start);
+
+                    RECT rect_TrayNotifyWnd;
+                    GetWindowRect(TrayNotifyWnd, &rect_TrayNotifyWnd);
 
                     RECT rect_MSTaskSwWClass;
                     GetWindowRect(MSTaskSwWClass, &rect_MSTaskSwWClass);
@@ -54,13 +59,20 @@ int main() //WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR 
                     int width_Shell_TrayWnd = (rect_Shell_TrayWnd.right - rect_Shell_TrayWnd.left);
                     int height_Shell_TrayWnd = (rect_Shell_TrayWnd.bottom - rect_Shell_TrayWnd.top);
 
-                    int left = abs(rect_MSTaskSwWClass.right - rect_Shell_TrayWnd.left + 2) * curDPI / 100;
+                    int left = abs(rect_Start.left - rect_Shell_TrayWnd.left + 2) * curDPI / 100;
                     int top = 2 * curDPI / 100;
-                    int right = abs(rect_MSTaskSwWClass.right - rect_Shell_TrayWnd.right + 1) * curDPI / 100;
+                    int right = abs(rect_MSTaskSwWClass.right - rect_Shell_TrayWnd.left + 1) * curDPI / 100;
                     int bottom = rect_MSTaskSwWClass.bottom * curDPI / 100;
 
                     HRGN region_ShellTrayWnd = CreateRoundRectRgn(left, top, right, bottom, 10, 10);
-                    SetWindowRgn(Shell_TrayWnd, region_ShellTrayWnd, TRUE);
+                    HRGN region_TrayNotifyWnd = CreateRoundRectRgn(abs(rect_TrayNotifyWnd.left - rect_Shell_TrayWnd.left + 2) * curDPI / 100, top, abs(rect_TrayNotifyWnd.right - rect_Shell_TrayWnd.left + 1) * curDPI / 100, bottom, 10, 10);
+
+                    HRGN region_Both = CreateRectRgn(0, 0, 0, 0);
+
+                    CombineRgn(region_Both, region_ShellTrayWnd, region_TrayNotifyWnd, RGN_OR);
+
+
+                    SetWindowRgn(Shell_TrayWnd, region_Both, TRUE);
 
                 }
 
@@ -68,15 +80,18 @@ int main() //WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR 
                 if (wcscmp(title, L"Shell_SecondaryTrayWnd") == 0) {
 
                     HWND Shell_SecondaryTrayWnd = tb;
-                    //HWND Start = FindWindowEx(Shell_SecondaryTrayWnd, 0, L"Start", NULL);
+                    HWND Start = FindWindowEx(Shell_SecondaryTrayWnd, 0, L"Start", NULL);
                     HWND WorkerW = FindWindowEx(Shell_SecondaryTrayWnd, 0, L"WorkerW", NULL);
                     HWND MSTaskListWClass = FindWindowEx(WorkerW, 0, L"MSTaskListWClass", NULL);
 
                     RECT rect_Shell_SecondaryTrayWnd;
                     GetWindowRect(Shell_SecondaryTrayWnd, &rect_Shell_SecondaryTrayWnd);
 
-                    //RECT rect_Start;
-                    //GetWindowRect(Start, &rect_Start);
+                    RECT rect_Start;
+                    GetWindowRect(Start, &rect_Start);
+
+                    RECT rect_WorkerW;
+                    GetWindowRect(WorkerW, &rect_WorkerW);
 
                     RECT rect_MSTaskListWClass;
                     GetWindowRect(MSTaskListWClass, &rect_MSTaskListWClass);
@@ -86,16 +101,55 @@ int main() //WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR 
                     int width_Shell_SecondaryTrayWnd = (rect_Shell_SecondaryTrayWnd.right - rect_Shell_SecondaryTrayWnd.left);
                     int height_Shell_SecondaryTrayWnd = (rect_Shell_SecondaryTrayWnd.bottom - rect_Shell_SecondaryTrayWnd.top);
 
-                    int left = abs(rect_MSTaskListWClass.right - rect_Shell_SecondaryTrayWnd.left + 2) * curDPI / 100;
-                    int top = 2 * curDPI / 100;
-                    int right = abs(rect_MSTaskListWClass.right - rect_Shell_SecondaryTrayWnd.right + 1) * curDPI / 100;
-                    int bottom = rect_MSTaskListWClass.bottom * curDPI / 100;
 
 
-                    //std::wcout << (left) << std::endl;
+                    HKEY hKey;
+                    DWORD buffer;
+                    LONG result;
+                    unsigned long type = REG_DWORD, size = 1024;
 
-                    HRGN region_Shell_SecondaryTrayWnd = CreateRoundRectRgn(left, top, right, bottom, 10, 10);
-                    SetWindowRgn(Shell_SecondaryTrayWnd, region_Shell_SecondaryTrayWnd, TRUE);
+                    result = RegOpenKeyEx(HKEY_CURRENT_USER, L"Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Advanced", 0, KEY_READ, &hKey);
+                    if (result == ERROR_SUCCESS)
+                    {
+                        RegQueryValueEx(hKey, L"TaskbarAl", NULL, &type, (LPBYTE)&buffer, &size);
+                        RegCloseKey(hKey);
+                        //printf("The value is :%d\n", buffer);
+                        //std::wcout << (buffer) << std::endl;
+
+
+                        //Center
+                        if (buffer == 1) {
+                            int left = abs(rect_MSTaskListWClass.right - rect_Shell_SecondaryTrayWnd.left + 2) * curDPI / 100;
+                            int top = 2 * curDPI / 100;
+                            int right = abs(rect_MSTaskListWClass.right - rect_Shell_SecondaryTrayWnd.right + 1) * curDPI / 100;
+                            int bottom = rect_MSTaskListWClass.bottom * curDPI / 100;
+
+                            HRGN region_Shell_SecondaryTrayWnd = CreateRoundRectRgn(left, top, right, bottom, 10, 10);
+                            SetWindowRgn(Shell_SecondaryTrayWnd, region_Shell_SecondaryTrayWnd, TRUE);
+                        }
+
+                        //Left
+                        if (buffer == 0) {
+                            int left = abs(rect_Start.left - rect_Shell_SecondaryTrayWnd.left + 2) * curDPI / 100;
+                            int top = 2 * curDPI / 100;
+                            int right = abs(rect_MSTaskListWClass.right - rect_Shell_SecondaryTrayWnd.left + 1) * curDPI / 100;
+                            int bottom = rect_MSTaskListWClass.bottom * curDPI / 100;
+
+                            HRGN region_Shell_SecondaryTrayWnd = CreateRoundRectRgn(left, top, right, bottom, 10, 10);
+                            SetWindowRgn(Shell_SecondaryTrayWnd, region_Shell_SecondaryTrayWnd, TRUE);
+                        }
+
+
+
+
+
+
+                    }
+
+
+
+
+                   
 
                 }
 
